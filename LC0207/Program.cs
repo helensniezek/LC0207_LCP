d8651a7b28fdf1125115b850b4beac3559b3b194
helensniezek <helensniezek@gmail.com>
2024-02-06 20:52:55 -0600

    Add project files.

---
+++ LC0207/Program.cs
@@ -0,0 +1,22 @@
+﻿// MIS 3013 001
+// Feburary 7, 2024
+// Helen Sniezek
+// 113552793
+
+// for loop
+// for (0;1;3)
+//{
+//    2;
+//}
+// 0: initialization
+// 1: conditional (true or false)
+// 2: loop body
+// 3: post processing
+// 0->1->3->2->
+//    1->3->2
+
+for(int i=20;i<=40 ;i=i+1 )
+{
+    //loop body
+    Console.WriteLine(i);
+}
